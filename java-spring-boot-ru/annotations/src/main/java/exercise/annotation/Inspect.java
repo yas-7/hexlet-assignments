0800bc24aaf36855fdf1e67852519531ee242ac6
@hexlet/cli <support@hexlet.io>
2025-08-13 18:59:38 +0300

    submit java-spring-boot-ru/annotations

---
+++ java-spring-boot-ru/annotations/src/main/java/exercise/annotation/Inspect.java
@@ -0,0 +1,10 @@
+package exercise.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+// BEGIN
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inspect {
+}
+// END
